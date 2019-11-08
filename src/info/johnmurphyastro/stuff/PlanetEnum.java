91b7f0ec5ea904dcab2c2cde82784157f13b6121
John <John@Astronomy.connect>
2019-11-08 15:45:20 +0000

    First commit

---
+++ src/info/johnmurphyastro/stuff/PlanetEnum.java
@@ -0,0 +1,16 @@
+package info.johnmurphyastro.stuff;
+
+/**
+ * Demonstrates an enum class
+ * @author John Murphy
+ */
+public enum PlanetEnum {
+    MERCURY,
+    VENUS,
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN,
+    URANUS,
+    NEPTUNE
+}
